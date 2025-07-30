82a727a1091e4a49e52223300599262258fca5d2
Ashish Singh <106435913+Ashish12022003@users.noreply.github.com>
2025-07-30 15:33:04 +0530

    Update LoginAction.java

--- LoginAction.java
+++ LoginAction.java
@@ -1,15 +1,34 @@
 package com.example;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import javax.servlet.http.*;
 import org.apache.struts.action.*;
 
 public class LoginAction extends Action {
     public ActionForward execute(ActionMapping mapping, ActionForm form,
-                                 HttpServletRequest request, HttpServletResponse response) {
+        HttpServletRequest request, HttpServletResponse response) {
+        
         LoginForm loginForm = (LoginForm) form;
+        boolean isValid = false;
 
-        if ("admin".equals(loginForm.getUsername()) && "123".equals(loginForm.getPassword())) {
+        try {
+            Connection conn = DBConnection.getConnection();
+            CallableStatement stmt = conn.prepareCall("{CALL check_login(?, ?, ?)}");
+            stmt.setString(1, loginForm.getUsername());
+            stmt.setString(2, loginForm.getPassword());
+            stmt.registerOutParameter(3, Types.INTEGER);
+            stmt.execute();
+
+            int result = stmt.getInt(3);
+            isValid = (result == 1);
+
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (isValid) {
             return mapping.findForward("success");
         } else {
             return mapping.findForward("failure");
