ec2f6432adee6a5966f5c3aaa76016d25fb8b999
Ashish Singh <106435913+Ashish12022003@users.noreply.github.com>
2025-07-31 13:28:06 +0530

    Update LoginForm.java

--- LoginForm.java
+++ LoginForm.java
@@ -5,6 +5,8 @@ import org.apache.struts.action.ActionForm;
 public class LoginForm extends ActionForm {
     private String username;
     private String password;
+    private String email;
+    private String role;
 
     public String getUsername() {
         return username;
@@ -18,5 +20,17 @@ public class LoginForm extends ActionForm {
     }
     public void setPassword(String password) {
         this.password = password;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getRole(){
+        return role;
+    }
+    public void setRole(String role){
+        this.role=role;
     }
 }
